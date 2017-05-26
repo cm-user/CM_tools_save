c3594b1e0d0cc61cd58f83163fb0246e5b36f50a
cm-user <antoine@cadeau-maestro.com>
2017-05-26 16:05:38 +0200

    reparation produit defectueux à declarer liste ordre alphabetique

--- src/FaultyProductBundle/Controller/FaultyController.php
+++ src/FaultyProductBundle/Controller/FaultyController.php
@@ -210,6 +210,30 @@ class FaultyController extends Controller
     }
 
     /**
+     * Deletes a faultyAction entity.
+     *
+     * @Route("/{id}", name="faulty_delete_byButton")
+     * @Method("DELETE")
+     */
+    public function delete_byButtonAction(Request $request, Faulty $faulty)
+    {
+
+            $productImages = $faulty->getProduct()->getProductImages();
+            $productImageRepository = $this->get('faulty.repository.product_image');
+
+
+            foreach ($productImages as $productImage) {
+                /* @var $productImage ProductImage */
+                $productImageRepository->delete($productImage);
+            }
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($faulty);
+            $em->flush();
+
+        $this->get('shop.repository.faulty')->delete($faulty);
+    }
+
+    /**
      * Creates a form to delete a faultyAction entity.
      *
      * @param Faulty $faulty The faultyAction entity
